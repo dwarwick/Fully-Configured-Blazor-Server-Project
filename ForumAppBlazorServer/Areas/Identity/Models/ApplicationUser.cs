78e62168688f2cffdf89590796be5c1e4ad53555
David Warwick <bgmsd@live.com>
2022-08-29 19:32:44 -0700

    Added first and last name to ApplicationUser model. Added link on NavBar to FetchData page, and added the Authorized attribute to the FetchData page so that it can only be accessed if the user is logged in. Implemented RedirectToLogin page so that when a user tries to navigate to a page that requires the user to be logged in, they are redirected to the login page instead. After they successfully log in, they are then redirected to the page they were trying to access, such as the FetchData page.

---
+++ ForumAppBlazorServer/Areas/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ForumAppBlazorServer.Areas.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+    }
+}
